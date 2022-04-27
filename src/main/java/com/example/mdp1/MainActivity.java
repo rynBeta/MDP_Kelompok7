dc510d2ce06b1d7c7091d08a1603df46541fce06
rynBeta <104483521+rynBeta@users.noreply.github.com>
2022-04-27 12:39:16 +0700

    Add files via upload

---
+++ src/main/java/com/example/mdp1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.mdp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    }
+}
